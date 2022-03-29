9e936a94e65701f3ec71ee7239812daff09db53a
Pertti Palo <giuthas@gmail.com>
2022-03-29 00:31:33 -0400

    Updated the beep file names to be more specific.

--- generate_beeps.m
+++ generate_beeps.m
@@ -1,8 +1,12 @@
 
 
-function generate_beeps(min, max, beep_len, beep_f, nro_beeps, seed)
+function generate_beeps(prefix, min, max, beep_len, beep_f, nro_beeps, seed)
 %gen_beeps_ex2 Generate stimulus beeps for experiment 2
-%   Arguments are (all in seconds) 
+%   Generate delayed beeps for naming experiments in AAA.
+%   Filenames will be [prefix]_[seed]_[running number].wav.
+%
+%   Arguments are (where relevant in seconds): 
+%   prefix = filename prefix from user. 
 %   min = minimum length of wait i.e. silence in the beginning
 %   max = maximum length of wait i.e. silence in the beginning
 %   beep_len = lenght of the stimulus beep
@@ -29,7 +33,7 @@ function generate_beeps(min, max, beep_len, beep_f, nro_beeps, seed)
     end
     
     for i = 1:nro_beeps
-        f_name = [sprintf('%0.3d', i) '.wav'];
+        f_name = [prefix sprintf('_%d_%0.3d', seed, i) '.wav'];
         delay = randi(interval, 1);
         audiowrite(f_name, audio(delay:end), fs);
     end
