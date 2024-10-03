d1a789f7f08ea9cfcdfb1a4789d49a69533110b9
Pertti Palo <giuthas@gmail.com>
2024-10-02 20:40:11 -0600

    Example run to show how the new generation system works.

---
+++ example_run.R
@@ -0,0 +1,32 @@
+# Run this to get write_short_session working.
+source("write_short_session.R")
+
+# Load the files and set the prefix and suffix below.
+words <- read.csv("stimuli.csv", header = F, sep = "\t", fill = F)
+words <- as.vector(words$V1)
+
+end_calibration <- read.csv("end_calibration.csv",
+    header = F, sep="\t", fill = F)
+end_calibration <- as.vector(end_calibration$V1)
+
+calibration <- read.csv("calibration.csv",
+    header = F, sep = "\t", fill = F) 
+calibration <- as.vector(calibration$V1)
+
+prefix = "test-stimulus-list-"
+suffix = ".csv"
+
+# The actual generation run.
+
+# Participant number is used both for numbering files and as the random seed.
+# Here we generate participants 1 and 2.
+write_short_session(
+    prefix, suffix, participant = 1,
+    words = words, calibration = calibration, end_calibration = end_calibration,
+    repeats = 2, half_way_break = T)
+write_short_session(
+    prefix, suffix, participant = 2,
+    words = words, calibration = calibration, end_calibration = end_calibration,
+    repeats = 2, half_way_break = T)
+
+# Remember to change the newlines with emacs (c-x ret f dos).
